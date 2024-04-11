57c67b547669e2308601ef5cd5ed3ea35dcea16c
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2024-04-11 11:42:04 +0530

    env vars setup

--- healthguardians/healthguardians/settings.py
+++ healthguardians/healthguardians/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k36lm4k)os_=@(saz#r+s)b*e0)x)ys$-3u5n8^@q_@d@e7(l7'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool, default=True)
 
 ALLOWED_HOSTS = []
 
