b169b2c816f9af493fcb74ca14fa3bdff94f6c7a
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2024-04-13 02:34:22 +0530

    model connection is done

--- healthguardians/api/views.py
+++ healthguardians/api/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.views import APIView
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import pandas as pd
+import os
+import joblib
 
-# Create your views here.
+print(os.path)
+model = joblib.load(os.path.join('./ML Model/Diabeties/diabetes_model.joblib'))
+scaler = StandardScaler()
+X = pd.read_csv('./ML Model/Diabeties/diabetes.csv').drop(columns = 'Outcome', axis=1)
+scaler.fit(X)
+
+class Test(APIView):		
+    def post(self, request):
+        input_data = pd.DataFrame(request.data, index=[0])
+
+        # standardize the input data
+        std_data = scaler.transform(input_data)
+
+        prediction = model.predict(std_data)[0]
+        if prediction == 0:
+            return JsonResponse({'diabetic':False})
+        else:
+            return JsonResponse({'diabetic':True})
+    
+    
+
+
+
